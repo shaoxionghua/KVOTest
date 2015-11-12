0e7d5ced5f04de0fe7e5a972483ee89f7748f7ea
当归 <danggui@jiayukang.com>
2015-11-12 09:46:55 +0800

    Initial Commit

---
+++ KVOTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVOTest
+//
+//  Created by danggui on 15/11/12.
+//  Copyright © 2015年 danggui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KVOTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KVOTest
+//
+//  Created by danggui on 15/11/12.
+//  Copyright © 2015年 danggui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
